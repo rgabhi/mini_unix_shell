06f6df63a1d4217219d8ae683256ac685ac5a335
rgabhi <abhinavrgupta98@gmail.com>
2025-12-06 18:16:50 +0530

    base main code

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+// 
+char **parse_line(char *line){
+
+    
+
+}
+
+int main(){
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+}
