20584ae9b64740ab163f75e8e6309a836bd586a7
rgabhi <abhinavrgupta98@gmail.com>
2025-12-08 10:14:02 +0530

    add: fork-exec logic

--- main.c
+++ main.c
@@ -2,9 +2,38 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include<string.h>
+#include <unistd.h>  // fork, execvp
+#include <sys/wait.h> //waitpid
 
 #define TOKEN_BUFF_SZ 64
-#define LSH_TOK_DELIM " \t\r\n\a"
+#define TOKEN_DELIMS " \t\r\n\a"
+
+int launch(char **args){
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if(pid == 0){
+        // child process
+        if(execvp(args[0], args) == -1){
+            //print error if cmd not found
+            perror("lsh");
+        }
+        // kill child if exec fails
+        exit(EXIT_FAILURE);
+    }
+    else if(pid < 0){
+        // error forking
+        perror("lsh");
+    }
+    else{
+        wpid = waitpid(pid, &status, WUNTRACED);
+        while(!WIFEXITED(status) && !WIFSIGNALED(status)){
+            wpid = waitpid(pid, &status, WUNTRACED);
+        }
+    }
+    return 1; // 1 to keep shell running 
+}
 
 char **tokenize_input(char *line){
     int bufsize = TOKEN_BUFF_SZ;
@@ -20,7 +49,7 @@ char **tokenize_input(char *line){
     }
 
     //2. get first token
-    token = strtok(line, LSH_TOK_DELIM);
+    token = strtok(line, TOKEN_DELIMS);
     while(token != NULL){
         tokens[position] = token;
         position++;
@@ -36,7 +65,7 @@ char **tokenize_input(char *line){
         }
 
         // 3. get next token- pass null
-        token = strtok(NULL, LSH_TOK_DELIM);
+        token = strtok(NULL, TOKEN_DELIMS);
 
     }
 
@@ -62,7 +91,7 @@ int main(){
 
         // 2. read line from stdin
         read = getline(&line, &len, stdin);
-
+        
         // 3. get args
         args = tokenize_input(line);
         if(args[0] != NULL){
